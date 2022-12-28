554b0c839ad5c9e155cc7abca87ae4357348946a
mosezhao <mosezhao@qq.cm>
2022-12-28 14:38:14 +0800

    #206160-msg-依据checklist，完成信用方山网站功能开发、部署、测试

--- src/SmartyConfig/Common/SmartyConfig.php
+++ src/SmartyConfig/Common/SmartyConfig.php
@@ -7,9 +7,9 @@ class SmartyConfig
     public static function configDir()
     {
         $dirArray = array(
-            S_ROOT.'vendor/qixinyun/zhongyang-share-config/src/SmartyConfig/Zy',
-            S_ROOT.'vendor/qixinyun/zhongyang-share-config/src/SmartyConfig/Common',
-            S_ROOT.'vendor/qixinyun/zhongyang-share-config/src/SmartyConfig/',
+            S_ROOT.'vendor/qixinyun/fangshan-share-config/src/SmartyConfig/Fs',
+            S_ROOT.'vendor/qixinyun/fangshan-share-config/src/SmartyConfig/Common',
+            S_ROOT.'vendor/qixinyun/fangshan-share-config/src/SmartyConfig/',
         );
 
         return $dirArray;
